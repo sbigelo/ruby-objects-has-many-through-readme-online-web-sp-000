322a1f4c4d2ed4fcc22f9e0942c39050455578d0
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-25 22:35:13 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -17,4 +17,11 @@ attr_accessor :name, :yrs_experience
     Meal.new(self, customer, total, tip)
   end
   
+  def meals
+  Meal.all.select do |meal|
+    meal.waiter == self #checking for waiter now
+    end
+  end
+  
+  
 end
